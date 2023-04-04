b8367444cc09ee8af031698cb1211d85fdd0e314
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-04-04 15:01:31 +0430

    Implement setter in item class!

--- item.rb
+++ item.rb
@@ -1,22 +1,26 @@
 class Item
-  attr_accessor :genre, :author
-  attr_reader :label, :archived, :publish_date
+  attr_accessor :label, :genre, :author, :archived, :publish_date
 
   def initialize(publish_date)
-    @id = Random.rand(1..100)
-    @genre = nil
-    @author = nil
-    @source = nil
-    @label = nil
+    @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = false
   end
 
-  attr_writer :source
-
-  def label=(new_label)
-    @label - new_label
+  def add_label(new_label)
+    @label = new_label
+    label.add_item(new_label)
   end
 
-  # place for archived methodes
+  def add_genre(new_genre)
+    @genre = new_genre
+    genre.add_item(new_genre)
+  end
+
+  def add_author(new_author)
+    @author = new_author
+    author.add_item(new_author)
+  end
+
+  # place for method
 end
