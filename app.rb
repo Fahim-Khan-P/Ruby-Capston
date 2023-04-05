76dc5dfc8f44126f615b657bb3db84c9fce29e03
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-04-05 20:30:58 +0430

    Add list book method in app class!

---
+++ app.rb
@@ -0,0 +1,43 @@
+require_relative './classes/lebal'
+require_relative './classes/book'
+require_relative './item'
+
+class App
+  puts 'Welcome to our App!'
+
+  def initialize
+    @books = []
+    @labels = []
+  end
+
+  def add_book
+    puts 'Enter Publisher:'
+    publish = gets.chomp
+    puts 'Enter Cover State:'
+    cover = gets.chomp
+    puts 'Enter publish Date:'
+    date = gets.chomp
+    book = Book.new(publish, cover, date)
+    @books.push(book)
+    puts 'Would you like to add label press (1):'
+    option = gets.chomp.to_i
+    return unless option == 1
+
+    puts 'Enter label\'s Title:'
+    title = gets.chomp
+    puts 'Enter label\'s color:'
+    color = gets.chomp
+    label = Label.new(title, color)
+    @labels.push(label)
+  end
+
+  def list_books
+    if @books.empty?
+      puts 'Sorry!, no saved books \n'
+    else
+      @books.each do |book|
+        puts "Publisher:#{book.publisher} Cover State: #{book.cover_state} Publish Date: #{book.publish_date} \n"
+      end
+    end
+  end
+end
