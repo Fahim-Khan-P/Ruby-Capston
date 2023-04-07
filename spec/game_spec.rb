d4e905457f4e4ac53146e67ec0c067bc71cf736f
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-04-08 00:48:33 +0430

    Add required changes!

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require_relative '../game'
 
 describe Game do
   before :each do
-    @game1 = Game.new Time.local(2022), 'should check the test_multiplayer', Time.local(2018)
+    @game1 = Game.new Time.local(2022), 'should check the test_multiplayer~', Time.local(2018)
     @game2 = Game.new Time.new(2009), Time.new(2018), true
   end
 
